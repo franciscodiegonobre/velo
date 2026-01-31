783fe2d697ebe47f480a42be563d1c0ae2eca18f
Francisco Nobre <franciscodiegonobre@gmail.com>
2026-01-31 11:43:16 +0100

    first commit

---
+++ playwright/e2e/online.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('webapp should be online', async ({ page }) => {
+  await page.goto('http://localhost:5173');
+
+  await expect(page).toHaveTitle(/Velô by Papito/);
+}); 
