d0a4a912c98f3ea051648c2362d551ff785f09d5
Francisco Nobre <franciscodiegonobre@gmail.com>
2026-02-01 19:54:20 +0100

    AAA pattern: Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,17 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('check approved order', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
-  // checkpoint 1
+  // AAA pattern: Arrange, Act, Assert
+  // Arrange
+  await page.goto('http://localhost:5173/lookup');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
-  
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-  // checkpoint 2
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-5QGHJX');
   await page.getByTestId('search-order-button').click();
 
-  // checkpoint 3
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible();
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-5QGHJX');
   await expect(page.getByTestId('order-result-status')).toBeVisible();
