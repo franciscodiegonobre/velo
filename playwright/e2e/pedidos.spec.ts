96b235d5a09a1a0f5d4722a2f574f12028a40e7a
Francisco Nobre <franciscodiegonobre@gmail.com>
2026-02-03 10:05:35 +0100

    playwright locators challenge

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,18 +3,21 @@ import { test, expect } from '@playwright/test';
 test('check approved order', async ({ page }) => {
   // AAA pattern: Arrange, Act, Assert
   // Arrange
-  await page.goto('http://localhost:5173/lookup');
+  await page.goto('http://localhost:5173');
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
   await page.getByRole('link', { name: 'Consultar Pedido' }).click();
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
 
   // Act
-  await page.getByTestId('search-order-id').fill('VLO-5QGHJX');
-  await page.getByTestId('search-order-button').click();
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-5QGHJX');
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
 
   // Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible();
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-5QGHJX');
-  await expect(page.getByTestId('order-result-status')).toBeVisible();
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+  // PW locator strategy
+  await expect(page.getByText('VLO-5QGHJX')).toBeVisible({ timeout: 10000 });
+  // Xpath locator
+  await expect(page.locator('//p[text()="VLO-5QGHJX"]')).toContainText('VLO-5QGHJX');
+  await expect(page.getByText('APROVADO')).toBeVisible();
+  // CSS locator
+  await expect(page.locator('div.text-green-700')).toContainText('APROVADO');
 });
